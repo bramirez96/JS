438338a42ac1f9c2e8bd371e42bb64b9e1b32b50
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-10-28 19:22:51 -0700

    made file

---
+++ Homework/Personal_Project/scripts/user_info_module.js
@@ -0,0 +1,8 @@
+/***
+* Meant to be called first in an inline event using "this" as parameter to initialize module.
+* Call this function every time hp, mp, xp, or level need to be updated (for now, might fix soon)
+*/
+
+function makeUserModule(container) {
+	if 
+}
